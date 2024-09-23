608591f6e5684b62f9321c457f3eb8dd00f2ce18
n1ckk0ch4n <visnevskaaveronika5@gmail.com>
2024-09-23 18:56:18 +0300

    Update Main.java

--- src/ru/vsu/cs/goryacheva/first/Main.java
+++ src/ru/vsu/cs/goryacheva/first/Main.java
@@ -12,9 +12,9 @@ public class Main {
         double y=scanner.nextDouble();
         System.out.print("input z");
         double z=scanner.nextDouble();
-        if (x!=0 && y!=0 && z!=0) System.out.printf("Point belongs to Oxyz");
-        else {if (x==0) System.out.println("Point belongs to Oyz");
-            if (y==0) System.out.println("Point belongs to Oxz");
-            if (z==0) System.out.println("Point belongs to Oxy");}
+        if (x!=0 && y!=0 && z!=0) System.out.printf("точка не принадлежит координатным плоскостям");
+        else {if (x==0) System.out.println("точка принадлежит Oyz");
+            if (y==0) System.out.println("точка принадлежит Oxz");
+            if (z==0) System.out.println("точка принадлежит Oxy");}
     }
 }
